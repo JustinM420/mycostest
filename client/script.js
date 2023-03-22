4b07101908041472b454f90512aac3f8ffcdeea1
Weedsociety <103718091+Weedsociety@users.noreply.github.com>
2023-03-21 19:48:14 -0700

    added login

--- client/script.js
+++ client/script.js
@@ -1,5 +1,12 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { firebaseConfig } from './firebaseConfig';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
@@ -7,6 +14,50 @@ const chatContainer = document.querySelector('#chat_container');
 let conversationHistory = [];
 
 let loadInterval;
+
+firebase.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+    // User is signed in, load conversation history
+    console.log('User signed in:', user.email);
+    conversationHistory = await loadConversationHistory(user);
+  } else {
+    // User is signed out
+    console.log('User signed out');
+  }
+});
+
+
+const registerForm = document.getElementById('register-form');
+registerForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value;
+
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    console.log('User registered');
+  } catch (error) {
+    console.error('Error registering user:', error.message);
+  }
+});
+
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
+
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+    console.log('User logged in');
+  } catch (error) {
+    console.error('Error logging in:', error.message);
+  }
+});
+
+
 
 function loader(element) {
   element.textContent = '';
@@ -64,15 +115,12 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
-  const userInput = data.get('prompt');
-  conversationHistory.push({ role: 'user', content: userInput });
-
   // User's chat stripe
-  chatContainer.innerHTML += chatStripe(false, userInput);
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
 
   form.reset();
 
-  //Bot's chatstripe
+  // Bot's chat stripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
@@ -82,32 +130,41 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  // Combine conversation history and the new user input into a single string
-  const historyText = conversationHistory
-    .map((message) => `${message.role === 'user' ? 'User:' : 'Bot:'} ${message.content}`)
-    .join('\n') + `\nUser: ${userInput}`;
-
-  // fetch data from server -> bot's response
+  // Fetch data from server -> bot's response
   const response = await fetch('https://mycos.onrender.com', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      historyText: historyText
-    })
+      prompt: `${conversationHistory.join('\n')}\nUser: ${data.get('prompt')}`,
+    }),
   });
 
   clearInterval(loadInterval);
   messageDiv.innerHTML = '';
 
   if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
-
-    conversationHistory.push({ role: 'bot', content: parsedData }); // Update conversation history
+    const responseData = await response.json();
+    const parsedData = responseData.bot.trim();
 
     typeText(messageDiv, parsedData);
+
+    // Update the conversation history
+    conversationHistory.push({
+      role: 'user',
+      message: data.get('prompt'),
+    });
+    conversationHistory.push({
+      role: 'bot',
+      message: parsedData,
+    });
+
+    // Store the conversation history for the logged-in user
+    const currentUser = firebase.auth().currentUser;
+    if (currentUser) {
+      await storeConversationHistory(currentUser, conversationHistory);
+    }
   } else {
     const err = await response.text();
 
@@ -115,7 +172,40 @@ const handleSubmit = async (e) => {
 
     alert(err);
   }
+};
+
+// load conversation history of user
+async function loadConversationHistory(user) {
+  const db = firebase.firestore();
+  const userRef = db.collection('users').doc(user.uid);
+
+  try {
+    const doc = await userRef.get();
+    if (doc.exists) {
+      return doc.data().conversation;
+    } else {
+      console.log('No conversation history found');
+      return [];
+    }
+  } catch (error) {
+    console.error('Error loading conversation history:', error.message);
+    return [];
+  }
 }
+
+// store conversation history of user
+async function storeConversationHistory(user, conversation) {
+  const db = firebase.firestore();
+  const userRef = db.collection('users').doc(user.uid);
+
+  try {
+    await userRef.set({ conversation });
+    console.log('Conversation history stored');
+  } catch (error) {
+    console.error('Error storing conversation history:', error.message);
+  }
+}
+
 
 
 form.addEventListener('submit', handleSubmit);
